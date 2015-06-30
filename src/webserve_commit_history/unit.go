7f17a2069878aa0727d722d6606631b4c8badc7c
hinst <hinstance@yandex.ru>
2015-06-30 22:18:13 +0300

    refactored; introduced GitCommitHistory object

--- src/webserve_commit_history/unit.go
+++ src/webserve_commit_history/unit.go
@@ -6,32 +6,30 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
-var Foo = 0
-
-func ReaderToString(reader io.Reader) string {
-	const chunkLength = 1024
-	var chunk []byte = make([]byte, chunkLength)
-	var text bytes.Buffer
-	for {
-		var readLength, readResult = reader.Read(chunk)
-		var chunkToWrite = chunk[:readLength]
-		text.Write(chunkToWrite)
-		if readResult == io.EOF {
-			break
-		}
-	}
-	return text.String()
+type GitCommitHistory struct {
+	Directory    string
+	CountOfItems int
+	TextFormat   string
 }
 
-func GetCommitHistoryAsText(directory string) string {
+const DefaultCountOfItems = 10
+const DefaultTextFormat = "%an @ %ad %n%B"
+
+func (this *GitCommitHistory) Init() {
+	this.CountOfItems = DefaultCountOfItems
+	this.TextFormat = DefaultTextFormat
+}
+
+func (this *GitCommitHistory) GetText() string {
 	var text = ""
-	var gitDirectory = directory + "/.git"
+	var gitDirectory = this.Directory + "/.git"
 	var _, gitStatResult = os.Stat(gitDirectory)
 	if gitStatResult == nil {
-		var command = exec.Command("/usr/bin/git", "log", "--pretty=format:%an @ %ad %n%B", "--max-count=10")
-		command.Dir = directory
+		var command = exec.Command("/usr/bin/git", "log", "--pretty=format:"+this.TextFormat, "--max-count="+strconv.Itoa(this.CountOfItems))
+		command.Dir = this.Directory
 		var output, outputResult = command.StdoutPipe()
 		if outputResult == nil {
 			var startResult = command.Start()
@@ -49,3 +47,20 @@ func GetCommitHistoryAsText(directory string) string {
 	}
 	return text
 }
+
+func ReaderToString(reader io.Reader) string {
+	const chunkLength = 1024
+	var chunk []byte = make([]byte, chunkLength)
+	var text bytes.Buffer
+	for {
+		var readLength, readResult = reader.Read(chunk)
+		var chunkToWrite = chunk[:readLength]
+		text.Write(chunkToWrite)
+		if readResult == io.EOF {
+			break
+		}
+	}
+	return text.String()
+}
+
+var Foo = 0
--- src/webserve_commit_history_app/app.go
+++ src/webserve_commit_history_app/app.go
@@ -8,5 +8,7 @@ import (
 var _ = webserve_commit_history.Foo
 
 func main() {
-	fmt.Println(webserve_commit_history.GetCommitHistoryAsText("/home/pi/hinst_static_website"))
+	var history webserve_commit_history.GitCommitHistory
+	history.Directory = "/home/pi/hinst_static_website"
+	fmt.Println(history.GetText())
 }
